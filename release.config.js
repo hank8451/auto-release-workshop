ad0fe6c2c22df74ca474268adc92c2b9e1216c6f
hank <hank.chen@cloras.io>
2024-02-25 16:07:50 +0800

    fix: update github url

--- release.config.js
+++ release.config.js
@@ -1,25 +1,29 @@
 module.exports = {
     branches: ['main'],
     plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      ['@semantic-release/npm',{npmPublish: false}],
-      [
-        '@semantic-release/changelog',
-        {
-          mangle: false,
-          headerIds: false,
-          changelogFile: 'CHANGELOG.md',
-        },
-      ],
-      [
-        '@semantic-release/git',
-        {
-          assets: ['CHANGELOG.md', 'package.json'],
-          message:
-            'chore(release): ${nextRelease.version} [skip ci]',
-        },
-      ],
-        '@semantic-release/github',
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        ['@semantic-release/npm', { npmPublish: false }],
+        [
+            '@semantic-release/changelog',
+            {
+                mangle: false,
+                headerIds: false,
+                changelogFile: 'CHANGELOG.md',
+            },
+        ],
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'package.json'],
+                message:
+                    'chore(release): ${nextRelease.version} [skip ci]',
+            },
+        ],
+        [
+            '@semantic-release/github',
+            {
+                githubUrl: "https://github.com/hank8451/auto-release-workshop.git"
+            }],
     ],
-  };
+};
